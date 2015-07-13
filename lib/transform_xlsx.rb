34efa40463092b4af85095bd4de8e897a6ef349f
Prathiba Kn <prathiba_kn@external.mckinsey.com>
2015-07-13 16:32:31 +0530

    Adding rspec

--- lib/transform_xlsx.rb
+++ lib/transform_xlsx.rb
@@ -2,6 +2,7 @@ require "transform_xlsx/version"
 require "transform_xlsx/data_type"
 require "transform_xlsx/single_dimensional"
 require "transform_xlsx/two_dimensional"
+
 module TransformXlsx
   # Your code goes here...
 end
---
+++ spec/two_dimensional_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+describe "" do
+  it "should " do
+    p "hello"
+  end
+end
